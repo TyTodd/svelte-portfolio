2ae3248516aa697c084388150a6c16b9b48a4a57
Ty Todd <swagginty@gmail.com>
2025-03-03 23:35:05 -0500

    fixed image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
+  // import yodamna from "%sveltekit.assets%/images/yodamna.jpg"
+  // import yodamna from "$static/images/yodamna.jpg"
 </script>
 <svelte:head>
   <title>Tyrin</title>
@@ -10,7 +12,7 @@
     Hello! My name is Tyrin. I am a senior in course 6-4 (AI and Decision
     Making). I'm from Pennsylvania and I have two cats.
 </p>
-<img src="/images/yodamna.jpg" alt="yodamna" />
+<img src="images/yodamna.jpg" alt="yodamna" />
 <h2>Latest Projects</h2>
 <div class="projects">
   {#each projects.slice(0, 3) as p}
